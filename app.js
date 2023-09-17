0f6a060df974b7df56654351accf51f37f38fc07
MotoPeter <klr650.club@yandex.ru>
2023-09-17 08:26:21 +0300

    добавлен мидлевер лимитед

--- app.js
+++ app.js
@@ -3,19 +3,22 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
-const router = require('./routes');
 const helmet = require('helmet');
+const router = require('./routes');
 const errorHandler = require('./middlewares/error-handler');
 require('dotenv').config();
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { cors } = require('./middlewares/cors');
+const { limiter } = require('./middlewares/limiter');
 
-const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/moviesdb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/moviesdb' } = process.env;
 
 const app = express();
 
 app.use(helmet());
+
+app.use(limiter);
 
 app.use(bodyParser.json());
 

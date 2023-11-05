8087143a8793962815899c2ca8b809f8de1e77c5
MotoPeter <klr650.club@yandex.ru>
2023-11-05 10:24:08 +0300

    исправления

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { cors } = require('./middlewares/cors');
 const { limiter } = require('./middlewares/limiter');
 
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/moviesdb' } = process.env;
+const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/moviesdb' } = process.env;
 
 const app = express();
 
--- middlewares/celebrate.js
+++ middlewares/celebrate.js
@@ -40,7 +40,7 @@ const createMovie = celebrate({
     duration: Joi.number().required(),
     year: Joi.number().required(),
     description: Joi.string().required(),
-    image: Joi.required(),
+    image: Joi.string().required().regex(regexUrl),
     trailerLink: Joi.string().required().regex(regexUrl),
     thumbnail: Joi.string().required().regex(regexUrl),
     movieId: Joi.number().required(),

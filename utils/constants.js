c952af8f7a8b2c6c8fea75871a8042e9c91c9c4a
MotoPeter <klr650.club@yandex.ru>
2023-11-06 15:23:25 +0300

    добавление доменов в корс

--- utils/constants.js
+++ utils/constants.js
@@ -5,6 +5,11 @@ const allowedCors = [
   'https://localhost:3001',
   'http://localhost:3000',
   'https://localhost:3000',
+  'https://me.motopeter.nomoredomainsrocks.ru',
+  'http://me.motopeter.nomoredomainsrocks.ru',
+  'https://api.me.motopeter.nomoredomainsrocks.ru',
+  'http://api.me.motopeter.nomoredomainsrocks.ru',
+
 ];
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
